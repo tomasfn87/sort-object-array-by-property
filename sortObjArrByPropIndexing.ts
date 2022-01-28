6cf42259f0e77378210a3a41be5e28988d6a37a6
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-01-28 20:27:37 -0300

    Adding file 'sortObjArrByPropIndexing': it contains the function 'sortObjectArrByPropIndexing', that sorts an array of objects (or an array of arrays) by one of their properties. The first argument is the object array. The second argument is the property whose values will be used to sort the whole array. The third argument is an optional argument, than can take either 's' (the default option) or 'r': 's' is for standard, ascendant order, and 'r' is for the reverse, descendant order. This function has 'Indexing' appended to its name because it does not order the input object array at first, but creates an index with the ordering pattern, and then orders the array. This function works faster with strings.

---
+++ sortObjArrByPropIndexing.ts
@@ -0,0 +1,34 @@
+export default function sortObjectArrByPropIndexing<Type>(
+  objArr: Type[],
+  objProp: string | number,
+  reverse: 's' | 'S' | 'r' | 'R' = 's'
+):Type[] {
+
+  let sortedObjectArray:Type[] = [];
+  let values:string[] | number[] | any[] = [];
+  let indices:number[] = [];
+
+  objArr.forEach((obj:string | number | any, index:number):void => {
+    typeof obj[objProp] === 'string'
+      || typeof obj[objProp] === 'number'
+      ? values.push(obj[objProp])
+      : values.push(obj[objProp].toString())
+    indices.push(index);
+  });
+
+  for (let i=1; i < values.length; i++) {
+    for (let j=0; j < values.length - i; j++) {
+      if (values[j] > values[j+1]) {
+        [values[j], values[j+1]] = [values[j+1], values[j]];
+        [indices[j], indices[j+1]] = [indices[j+1], indices[j]];
+      }
+    }
+  }
+
+  for (let k of indices) {
+    sortedObjectArray.push(objArr[k]);
+  }
+
+  reverse === 'r'.toLowerCase() && sortedObjectArray.reverse();
+  return sortedObjectArray;
+}
