d81c9d5a4d0753e4a5d21ba04cb2ff0d1581f5d5
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-01-28 20:29:45 -0300

    Adding file 'sortObjArrByPropDirectly': it contains the function 'sortObjectArrByPropIndexing', that sorts an array of object (or an array of arrays) by one of its properties. The first argument is the object array. The second argument is the property whose values will be used to sort the whole array. The third argument is an optional argument, than can take either 's' (the default option) or 'r': 's' is for standard, ascendant order, and 'r' is for the reverse, descendant order. This function has 'Directly' appended to its name because it orders the input object array in parallel with a list of values extracted from the input object array. This function works faster with numbers.

---
+++ sortObjArrByPropDirectly.ts
@@ -0,0 +1,30 @@
+export default function sortObjectArrByPropDirectly<Type>(
+  objArr: Type[],
+  objProp: string | number,
+  reverse: 's' | 'S' | 'r' | 'R' = 's'
+):Type[] {
+
+  let sortedObjectArray:Type[] = objArr;
+  let values:string[] | number[] | any[] = [];
+
+  objArr.forEach((obj:string | number | any):void => {
+    typeof obj[objProp] === 'string'
+      || typeof obj[objProp] === 'number'
+      ? values.push(obj[objProp])
+      : values.push(obj[objProp].toString())
+  });
+
+  for (let i=1; i < values.length; i++) {
+    for (let j=0; j < values.length - i; j++) {
+      if (values[j] > values[j+1]) {
+        [ values[j], values[j+1] ] = [ values[j+1], values[j] ];
+        [ sortedObjectArray[j], sortedObjectArray[j+1] ] = [
+          sortedObjectArray[j+1], sortedObjectArray[j]
+        ];
+      }
+    }
+  }
+
+  reverse === 'r'.toLowerCase() && sortedObjectArray.reverse();
+  return sortedObjectArray;
+}
