251993b4c50b29792c7ba9eb209b766c1bd18f95
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-01-31 09:40:13 -0300

    Changed file: 'sortObjArrByProps': added comments to explain the algorithm's steps.

--- sortObjArrByProps.ts
+++ sortObjArrByProps.ts
@@ -5,22 +5,23 @@ export default function sortObjectArrByProps<Type>(
   ):Type[] | any {
   
   let sortedObjectArray:Type[] = [];
-  let values:string[] | number[] | string[][] | number[][] | any[] = [];
+  let values:string[] | number[] | string[][] | number[][] | any[] | any[][] = [];
   let indices:number[] = [];
   
-  if (Array.isArray(objProps)) { // an array of props as sorting parameters
-    // 1) create empty array and indices
+  if (Array.isArray(objProps)) { // sort by multiple properties
+    // 1) create an empty array and an index for each item of argument objArr
     for (let i=0; i < objArr.length; i++) {
       values.push([]);
       indices.push(i);
     }
 
+    // 2) add argument 'objProps' values to each item of 'values'
     for (let j=0; j < objArr.length; j++) {
       for (let k=0; k < objProps.length; k++) {
         values[j].push(objArr[j][objProps[k]])
       }
     }
-  } else { // a single number or strings as sorting parameter
+  } else { // sort by one property
     objArr.forEach((obj:Type | any, index:number):void => {
       typeof objProps === 'string'
       || typeof objProps === 'number'
@@ -30,40 +31,40 @@ export default function sortObjectArrByProps<Type>(
     });
   }
 
-  if (!Array.isArray(objProps)) {
-    for (let i=1; i < values.length; i++) {
-      for (let j=0; j < values.length - i; j++) {
-        if (values[j] > values[j+1]) {
-          [values[j], values[j+1]] = [values[j+1], values[j]];
-          [indices[j], indices[j+1]] = [indices[j+1], indices[j]];
+  if (!Array.isArray(objProps)) {                                  // actually sorting by one property
+    for (let i=1; i < values.length; i++) {                        // sort optimization: at each iteration, the last value will be skipped (i)
+      for (let j=0; j < values.length - i; j++) {                  // values index
+        if (values[j] > values[j+1]) {                             // only case: value is greater than
+          [values[j], values[j+1]] = [values[j+1], values[j]];     // switch values
+          [indices[j], indices[j+1]] = [indices[j+1], indices[j]]; // switch indices
         }
       }
     }
-  } else if (!!Array.isArray(objProps)) {
-    let k = 0;
-    for (let i=1; i < objArr.length; i++) {
-      for (let j=0; j < objArr.length - i;) {
-        if (values[j][k] === values[j+1][k]) {
-          if (k === objProps.length - 1) {
-            k = 0; j++;
-          } else {
-            k++;
+  } else if (!!Array.isArray(objProps)) {      // actually sorting by multiple properties
+    let k = 0;                                 // objProps index
+    for (let i=1; i < values.length; i++) {    // sort optimization: at each iteration, the last value will be skipped (i)
+      for (let j=0; j < values.length - i;) {  // values index
+        if (values[j][k] === values[j+1][k]) { // 1) values are equal:
+          if (k === objProps.length - 1) {     // 1.1) all properties values are equal, can't sort
+            k = 0; j++;                        // 1.1.1) restart properties; check next value
+          } else {                             // 1.2) there are still properties to be checked
+            k++;                               // 1.2.1) check next property
           }
-        } else if (values[j][k] > values[j+1][k]) {
-          [values[j], values[j+1]] = [values[j+1], values[j]];
-          [indices[j], indices[j+1]] = [indices[j+1], indices[j]];
-          k = 0; j++;
-        } else if (values[j][k] < values[j+1][k]) {
-          j++;
+        } else if (values[j][k] > values[j+1][k]) {                // 2) values are different, the first one is greater:
+          [values[j], values[j+1]] = [values[j+1], values[j]];     // 2.1) switch values
+          [indices[j], indices[j+1]] = [indices[j+1], indices[j]]; // 2.3) switch indices
+          k = 0; j++;                                              // 2.4) restart properties; check next value
+        } else if (values[j][k] < values[j+1][k]) { // 3) values are different, the first one is smaller = already sorted
+          j++;                                      // 3.1) check next value
         }
       }
     }
   } 
 
-  for (let i of indices) {
-      sortedObjectArray.push(objArr[i]);
+  for (let i of indices) {                // populate the return variable sortedObjArr
+      sortedObjectArray.push(objArr[i]);  // add each of the received object array's items according to 'indices'
   }
 
-  reverse === 'r'.toLowerCase() && sortedObjectArray.reverse();
+  reverse === 'r'.toLowerCase() && sortedObjectArray.reverse(); // use native method to deliver a reversed list
   return sortedObjectArray;
 };
