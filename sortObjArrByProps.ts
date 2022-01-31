72eefbff2af8fe2521f8123069a30e5023ec7e4d
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-01-31 18:20:44 -0300

    Changed file: 'sortObjArrByProps.ts': added type 'values' to better organize the types of variable 'values'.

--- sortObjArrByProps.ts
+++ sortObjArrByProps.ts
@@ -1,13 +1,16 @@
-export default function sortObjectArrByProps<Type>(                  // 0) Sort Object Arrays By Property takes:
+export default function sortObjectArrByProps<Type>(                  // 0) Sort Object Array By Properties takes:
   objArr: Type[] | any[],                                            // 0.1) an array of objects or an array of arrays, that will be sorted according to:
-  objProps: string | number | string[] | number[],                   // 0.2.1) two or all of its properties: 0.2.1.1) an array of numbers, 0.2.1.1) an array of strings; or 0.2.2) a single property: 0.2.2.1) a number, 0.2.2.2) a string
-  reverse: 's' | 'S' | 'r' | 'R' = 's'                               // 0.3.1) 's' -> standard, ascending order, 0.3.2) 'r' -> reverse, descending order: will reverse 'sortedObjectArray'
-  ):Type[] | any[] {                                                 // 0.4) will return the items 'objArr' ordered by 'objProps'
+  objProps:                                                          // 0.2) 0.2.1) two or all of its properties, 0.2.2) a single property
+    string[] | number[] |                                            // 0.2.1.1) an array of strings, 0.2.1.2) an array of numbers
+    string | number,                                                 // 0.2.2.1) a number, 0.2.2.2) a string
+  reverse: 's' | 'S' | 'r' | 'R' = 's'                               // 0.3.1) 's' -> standard, ascending order; 0.3.2) 'r' -> reverse, descending order: will reverse 'sortedObjectArray'
+  ):Type[] | any[] {                                                 // 0.4) will return the items of 'objArr' ordered using 'objProps', leaving the original array untouched
 
-  type value = string | number | any                                 // type definition: 'value'
+  type value = string | number | any                                 // type definition: * 'value'
+  type values = value[] | value[][]                                  //                  * 'values'
 
   let sortedObjectArray:Type[] = [];                                 // will be used to mount the ordered array after ordering a list of values (2) or a list of lists of values (1)
-  let values:value[] | value[][] = [];                               // will store the values used to sort the list; may sort all values if all properties are informed to 'objProps'
+  let values:values = [];                                            // will store the values used to sort the list; may sort all values if all properties are informed to 'objProps'
   let indices:number[] = [];                                         // will store the indices of the objects or arrays to mount 'sortedObjectArray' before the function returns
   
   if (Array.isArray(objProps)) {                                     // 1) Sort by multiple properties
@@ -17,23 +20,23 @@ export default function sortObjectArrByProps<Type>(                  // 0) Sort 
     }
     for (let j=0; j < objArr.length; j++) {                          // 1.2) for each item of argument 'objArr'
       for (let k=0; k < objProps.length; k++) {                      // 1.2.1) for each item of argument 'objProps'
-        values[j].push(objArr[j][objProps[k]])                       // 1.2.1.1) add argument 'objArr's object's value for the current index and property
+        values[j].push(objArr[j][objProps[k]])                       // 1.2.1.1) add argument 'objArr's object's value for the current item (j) and property (k)
       }
     }
   } else {                                                           // 2) Sort by a single property
     objArr.forEach((obj:Type | any, index:number):void => {          // 2.1) for each item of argument 'objArr'
-      typeof objProps === 'string'                                   // 2.1.1) if objProps receives a string
+      typeof objProps === 'string'                                   // 2.1.1) if 'objProps' receives a string
       || typeof objProps === 'number'                                // 2.1.1') or a number
-        ? values.push(obj[objProps])                                 // 2.1.1.1) add it to 'values'
-        : values.push(obj[objProps].toString());                     // 2.1.1.2) else turn it into a string and add it to 'values'
-      indices.push(index);                                           // 2.1.2) push each items index to 'indices'
+        ? values.push(obj[objProps])                                 // 2.1.1.1) add it to 'values' as it is
+        : values.push(obj[objProps].toString());                     // 2.1.1.2) else (try to) turn it into a string and add it to 'values'
+      indices.push(index);                                           // 2.1.2) add each item's index to 'indices'
     });
   }
 
   if (!Array.isArray(objProps)) {                                    // 2.2) Sorting by a single property - swap logic
-    for (let i=1; i < values.length; i++) {                          // 2.2.1) sort optimization: at each iteration, the last value will be skipped (i)
+    for (let i=1; i < values.length; i++) {                          // 2.2.1) sort optimization: at each iteration, the last value will be skipped (i++)
       for (let j=0; j < values.length - i; j++) {                    // 2.2.2) 'values' index
-        if (values[j] > values[j+1]) {                               // 2.2.3) only case: first value is greater than second value
+        if (values[j] > values[j+1]) {                               // 2.2.3) Single case: first value is greater than second value
           [values[j], values[j+1]] = [values[j+1], values[j]];       // 2.2.3.1) switch 'values'
           [indices[j], indices[j+1]] = [indices[j+1], indices[j]];   // 2.2.3.2) switch 'indices'
         }
@@ -43,17 +46,17 @@ export default function sortObjectArrByProps<Type>(                  // 0) Sort 
     let k = 0;                                                       // 1.3.1) starting 'objProps' index
     for (let i=1; i < values.length; i++) {                          // 1.3.2) sort optimization: at each iteration, the last value will be skipped (i)
       for (let j=0; j < values.length - i;) {                        // 1.3.2.1) 'values' index
-        if (values[j][k] === values[j+1][k]) {                       // 1.3.2.1.1) objects' values are equal
+        if (values[j][k] === values[j+1][k]) {                       // 1.3.2.1.1) Case one: objects' values are equal
           if (k === objProps.length - 1) {                           // 1.3.2.1.1.1) all properties values are equal, can't sort
             k = 0; j++;                                              // 1.3.2.1.1.1.1) restart properties; check next value
           } else {                                                   // 1.3.2.1.1.2) there are still properties to be checked
             k++;                                                     // 1.3.2.1.1.2.1) check next property
           }
-        } else if (values[j][k] > values[j+1][k]) {                  // 1.3.2.1.2) first object's value > second object's value: swap the two values
+        } else if (values[j][k] > values[j+1][k]) {                  // 1.3.2.1.2) Case two: first object's value > second object's value: swap the two values
           [values[j], values[j+1]] = [values[j+1], values[j]];       // 1.3.2.1.2.1) switch 'values'
           [indices[j], indices[j+1]] = [indices[j+1], indices[j]];   // 1.3.2.1.2.2) switch 'indices'
           k = 0; j++;                                                // 1.3.2.1.2.3) restart properties; check next value
-        } else if (values[j][k] < values[j+1][k]) {                  // 1.3.2.1.3) first object's value < second object's value: they're already sorted
+        } else if (values[j][k] < values[j+1][k]) {                  // 1.3.2.1.3) Case three: first object's value < second object's value: means already sorted
           j++;                                                       // 1.3.2.1.3.1) check next value
         }
       }
