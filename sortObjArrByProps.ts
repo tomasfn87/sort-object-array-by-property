f64cf0d9b2a7ecd47504b6e6e00fbcf996298496
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-02-01 19:14:48 -0300

    Changed file 'test/03-sortObjArrByProps.test.ts': added tests for a list of objects with different properties.

--- sortObjArrByProps.ts
+++ sortObjArrByProps.ts
@@ -1,23 +1,23 @@
-type value = string | number | any;                                            // ideally a string or a number through which the list will be sorted, or some kind of data that can be turned into a string
-type values = value[] | value[][];                                             // an array of 'value' or an array of arrays of 'value', used to sort a 'indices'
-type index = number;                                                           // a number used to index a position in an array
-type indices = index[];                                                        // an array of 'index'
-type prop = string | index                                                     // a string for objects; an 'index' for arrays
-type obj = {} | value[]                                                        // an object or an array
-type objArr = readonly obj[]                                                   // a readonly array of objects or arrays
+type value = string | number | boolean | any                                      // ideally a string or a number through which the list will be sorted, or some kind of data that can be turned into a string
+type values = value[] | value[][]                                                 // an array of 'value' or an array of arrays of 'value', used to sort a 'indices', using only one or all properties of the objects in 'objArr'
+type index = number                                                               // a number used to index a position in an array
+type indices = index[]                                                            // an array of 'index'
+type prop = string | index                                                        // a string for objects; an 'index' for arrays
+type obj = {} | value[]                                                           // an object or an array
+type objArr = readonly obj[] | any[]                                              // a read only array of objects or arrays
 
-export default function sortObjectArrByProps<Type>(                            // 0) Sort Object Array By Properties takes:
-  objArr: objArr | any[],                                                      // 0.1) an array of objects or an array of arrays, that will be sorted according to:
-  objProps:                                                                    // 0.2) 0.2.1) between two and all their properties' values, 0.2.2) a single property's values
-    prop[]                                                                     // 0.2.1.1) an array of strings, 0.2.1.2) an array of numbers
-    | prop,                                                                    // 0.2.2.1) a number, 0.2.2.2) a string
-  reverse: 's' | 'S' | 'r' | 'R' = 's'                                         // 0.3.1) 's' -> standard, ascending order; 0.3.2) 'r' -> reverse, descending order: will reverse 'sortedObjectArray'
-  ):Type[] {                                                                   // 0.4) will return the items of 'objArr' ordered using 'objProps', leaving the original array untouched
+export default function sortObjectArrByProps<Type>(                               // 0) Sort Object Array By Properties takes:
+  objArr: objArr,                                                                 // 0.1) an array of objects or an array of arrays, that will be sorted according to:
+  objProps:                                                                       // 0.2) 0.2.1) between two and all their properties' values, 0.2.2) a single property's values
+    prop[]                                                                        // 0.2.1.1) an array of strings, 0.2.1.2) an array of numbers
+    | prop,                                                                       // 0.2.2.1) a number, 0.2.2.2) a string
+  reverse: 's' | 'S' | 'r' | 'R' = 's'                                            // 0.3.1) 's' -> standard, ascending order; 0.3.2) 'r' -> reverse, descending order: will reverse 'sortedObjectArray'
+  ):Type[] {                                                                      // 0.4) will return the items of 'objArr' ordered using 'objProps', leaving the original array untouched
 
-  let sortedObjectArray:Type[] = [];                                           // will be used to mount the ordered array after ordering a list of values (2) or a list of lists of values (1)
-  let values:values | any = [];                                                // will store the values according to which the list will be sorted; may sort according to all values if all properties are informed to 'objProps'
-  let indices:indices = [];                                                    // will store the indices of the objects or arrays to mount 'sortedObjectArray' before the function returns
-  let missingProps:boolean = false;                                            // will store a boolean that tells if one or more properties are missing from one of the objects or arrays in 'objArr'
+  let sortedObjectArray:Type[] = [];                                              // will be used to mount the ordered array after ordering a list of values (2) or a list of lists of values (1)
+  let values:values | any = [];                                                   // will store the values according to which the list will be sorted; may be all values if all objects' properties are passed to 'objProps'
+  let indices:indices = [];                                                       // will store the indices of the objects or arrays to mount 'sortedObjectArray' before the function returns
+  let missingProps:boolean = false;                                               // will store a boolean that tells if one or more properties are missing from one of the objects or arrays in 'objArr'
   
   if (!!Array.isArray(objProps)) {                                                // 1) Sort by two or all properties
     for (let i=0; i < objArr.length; i++) {                                       // 1.1) for each item of argument 'objArr'
@@ -45,7 +45,7 @@ export default function sortObjectArrByProps<Type>(                            /
         values.push('⭢')                                                          // 2.1.2.1) add '⭢' to values
         missingProps = true;                                                      // 2.1.2.2) set 'missingProps' to true; string comparison is triggered
       }
-      indices.push(i);                                                         // 2.1.3) add each item's index to 'indices'
+      indices.push(i);                                                            // 2.1.3) add each item's index to 'indices'
     });
   }
 
@@ -60,27 +60,27 @@ export default function sortObjectArrByProps<Type>(                            /
             } else {                                                              // 1.3.2.1.1.1.2) there are still properties to be checked
               k++;                                                                // 1.3.2.1.1.1.2.1) check next property
             }
-          } else if (values[j][k] > values[j+1][k]) {                          // 1.3.2.1.2) Case two: first object's value > second object's value: swap the two values
-            [values[j], values[j+1]] = [values[j+1], values[j]];               // 1.3.2.1.2.1) switch 'values'
-            [indices[j], indices[j+1]] = [indices[j+1], indices[j]];           // 1.3.2.1.2.2) switch 'indices'
-            k = 0; j++;                                                        // 1.3.2.1.2.3) restart properties; check next value
-          } else {                                                             // 1.3.2.1.3) Case three: first object's value < second object's value: already sorted, nothing to do
-            k = 0; j++;                                                        // 1.3.2.1.3.1) restart properties; check next value
+          } else if (values[j][k] > values[j+1][k]) {                             // 1.3.2.1.1.1) Case two: first value > second value: swap the two values
+            [values[j], values[j+1]] = [values[j+1], values[j]];                  // 1.3.2.1.1.2.1) swap 'values'
+            [indices[j], indices[j+1]] = [indices[j+1], indices[j]];              // 1.3.2.1.1.2.2) swap 'indices'
+            k = 0; j++;                                                           // 1.3.2.1.1.2.3) restart properties; check next value
+          } else {                                                                // 1.3.2.1.1.3) Case three: first value < second value: already sorted, nothing to do
+            k = 0; j++;                                                           // 1.3.2.1.1.3.1) restart properties; check next value
           }
         }
-        else {                                                                 // 1.2.2.1.2) if there are missing values, string checking:
-          if (values[j][k].toString() === values[j+1][k].toString()) {         // 1.3.2.1.2.1) Case one: objects' values are equal
-            if (k === objProps.length - 1) {                                   // 1.3.2.1.2.1.1) all properties values are equal, can't sort
-              k = 0; j++;                                                      // 1.3.2.1.2.1.1.1) restart properties; check next value
-            } else {                                                           // 1.3.2.1.2.1.2) there are still properties to be checked
-              k++;                                                             // 1.3.2.1.2.1.2.1) check next property
+        else {                                                                    // 1.2.2.1.2) else, some objects in 'objArr' don't have all properties listed in 'objProps'
+          if (values[j][k].toString() === values[j+1][k].toString()) {            // 1.3.2.1.2.1) Case one: values are equal; using string check to test numbers or strings against ''
+            if (k === objProps.length - 1) {                                      // 1.3.2.1.2.1.1) all properties values are equal, can't sort
+              k = 0; j++;                                                         // 1.3.2.1.2.1.1.1) restart properties; check next value
+            } else {                                                              // 1.3.2.1.2.1.2) there are still properties to be checked
+              k++;                                                                // 1.3.2.1.2.1.2.1) check next property
             }
-          } else if (values[j][k].toString() > values[j+1][k].toString()) {    // 1.3.2.1.2) Case two: first object's value > second object's value: swap the two values
-            [values[j], values[j+1]] = [values[j+1], values[j]];               // 1.3.2.1.2.1) switch 'values'
-            [indices[j], indices[j+1]] = [indices[j+1], indices[j]];           // 1.3.2.1.2.2) switch 'indices'
-            k = 0; j++;                                                        // 1.3.2.1.2.3) restart properties; check next value
-          } else {                                                             // 1.3.2.1.3) Case three: first object's value < second object's value: already sorted, nothing to do
-            k = 0; j++;                                                        // 1.3.2.1.3.1) restart properties; check next value
+          } else if (values[j][k].toString() > values[j+1][k].toString()) {       // 1.3.2.1.2.2) Case two: first value > second value: swap the two values; using string comparison to test numbers or strings against ''
+            [values[j], values[j+1]] = [values[j+1], values[j]];                  // 1.3.2.1.2.2.1) swap 'values'
+            [indices[j], indices[j+1]] = [indices[j+1], indices[j]];              // 1.3.2.1.2.2.2) swap 'indices'
+            k = 0; j++;                                                           // 1.3.2.1.2.2.3) restart properties; check next value
+          } else {                                                                // 1.3.2.1.2.3) Case three: first value < second value: already sorted, nothing to do
+            k = 0; j++;                                                           // 1.3.2.1.2.3.1) restart properties; check next value
           }
         }
       }
@@ -103,10 +103,10 @@ export default function sortObjectArrByProps<Type>(                            /
     }
   }
 
-  for (let i of indices) {                                                     // *.1) populate 'sortedObjArr'
-      sortedObjectArray.push(objArr[i]);                                       // *.1.1) add each of the received object array's items following 'indices' values
+  for (let i of indices) {                                                        // *.1) populate 'sortedObjArr'
+      sortedObjectArray.push(objArr[i]);                                          // *.1.1) add each of the received object array's items following 'indices' values
   }
 
-  reverse === 'r'.toLowerCase() && sortedObjectArray.reverse();                // *.2) if 'reverse' receives 'r' or 'R' as argument, 'sortedObjectArray' is reversed
-  return sortedObjectArray;                                                    // *.2.1) return a copy of 'objArr' sorted by 'objProps'
-};
+  reverse === 'r'.toLowerCase() && sortedObjectArray.reverse();                   // *.2) if 'reverse' receives 'r' or 'R' as argument, 'sortedObjectArray' is reversed
+  return sortedObjectArray;                                                       // *.2.1) return a copy of 'objArr' sorted by 'objProps'
+}
--- test/03-sortObjArrByProps.test.ts
+++ test/03-sortObjArrByProps.test.ts
@@ -39,6 +39,22 @@ const objArr2 = [
   { a: 1, b: 2, c: 4 }
 ]
 
+const objArr3 = [
+  { a: 1, b: 1, c: 2 },
+  { a: 4, b: 1, c: 1 },
+  { a: 3, b: 2, c: 2 },
+  { a: 1, b: 1 },
+  { a: 1, b: 1, c: 3 },
+  { a: 1, b: 1, c: 4 },
+  { a: 1, b: 2, c: 2 },
+  { a: 2, b: 3, c: 3 },
+  { a: 1, b: 2, c: 1 },
+  { a: 1 },
+  { a: 1, b: 2, c: 3 },
+  { a: 1, b: 1, c: 1 },
+  { a: 1, b: 2, c: 4 }
+]
+
 describe('3) Sort Object Array by Properties, takes an array of properties whose items will be used as sorting parameters.', () => {
   it(`3.1) ../sortObjArrByProps.ts.sortObjectArrByProps(
     [                    ->    [
@@ -257,6 +273,76 @@ describe('3) Sort Object Array by Properties, takes an array of properties whose
       { a: 1, b: 2, c: 3 },
       { a: 1, b: 2, c: 2 },
       { a: 1, b: 2, c: 1 },
+      { a: 1, b: 1, c: 4 },
+      { a: 1, b: 1, c: 3 },
+      { a: 1, b: 1, c: 2 },
+      { a: 1, b: 1, c: 1 }
+    ]);
+  });
+
+  it(`3.9) ../sortObjArrByProps.ts.sortObjectArrByProps(
+    [                          ->    [
+      { a: 1, b: 1, c: 2 },    ->      { a: 1, b: 1, c: 1 },
+      { a: 4, b: 1, c: 1 },    ->      { a: 1, b: 1, c: 2 },
+      { a: 3, b: 2, c: 2 },    ->      { a: 1, b: 1, c: 3 },
+      { a: 1, b: 1 },          ->      { a: 1, b: 1, c: 4 },
+      { a: 1, b: 1, c: 3 },    ->      { a: 1, b: 1 },
+      { a: 1, b: 1, c: 4 },    ->      { a: 1, b: 2, c: 1 },
+      { a: 1, b: 2, c: 2 },    ->      { a: 1, b: 2, c: 2 },
+      { a: 2, b: 3, c: 3 },    ->      { a: 1, b: 2, c: 3 },
+      { a: 1, b: 2, c: 1 },    ->      { a: 1, b: 2, c: 4 },
+      { a: 1 },                ->      { a: 1 },
+      { a: 1, b: 2, c: 3 },    ->      { a: 2, b: 3, c: 3 },
+      { a: 1, b: 1, c: 1 },    ->      { a: 3, b: 2, c: 2 },
+      { a: 1, b: 2, c: 4 }     ->      { a: 4, b: 1, c: 1 }
+    ]                          ->    [ 
+  ['a', 'b', 'c'])`, () => {
+    let result = sortObjectArrByProps(objArr3, ['a', 'b', 'c']);
+    expect(result).to.eql([
+      { a: 1, b: 1, c: 1 },
+      { a: 1, b: 1, c: 2 },
+      { a: 1, b: 1, c: 3 },
+      { a: 1, b: 1, c: 4 },
+      { a: 1, b: 1 },
+      { a: 1, b: 2, c: 1 },
+      { a: 1, b: 2, c: 2 },
+      { a: 1, b: 2, c: 3 },
+      { a: 1, b: 2, c: 4 },  
+      { a: 1 },
+      { a: 2, b: 3, c: 3 },
+      { a: 3, b: 2, c: 2 },
+      { a: 4, b: 1, c: 1 }
+    ]);
+  });
+
+  it(`3.10) ../sortObjArrByProps.ts.sortObjectArrByProps(
+    [                          ->    [
+      { a: 1, b: 1, c: 2 },    ->      { a: 4, b: 1, c: 1 },
+      { a: 4, b: 1, c: 1 },    ->      { a: 3, b: 2, c: 2 },
+      { a: 3, b: 2, c: 2 },    ->      { a: 2, b: 3, c: 3 },
+      { a: 1, b: 1 },          ->      { a: 1 },
+      { a: 1, b: 1, c: 3 },    ->      { a: 1, b: 2, c: 4 },
+      { a: 1, b: 1, c: 4 },    ->      { a: 1, b: 2, c: 3 },
+      { a: 1, b: 2, c: 2 },    ->      { a: 1, b: 2, c: 2 },
+      { a: 2, b: 3, c: 3 },    ->      { a: 1, b: 2, c: 1 },
+      { a: 1, b: 2, c: 1 },    ->      { a: 1, b: 1 },
+      { a: 1 },                ->      { a: 1, b: 1, c: 4 },
+      { a: 1, b: 2, c: 3 },    ->      { a: 1, b: 1, c: 3 },
+      { a: 1, b: 1, c: 1 },    ->      { a: 1, b: 1, c: 2 },
+      { a: 1, b: 2, c: 4 }     ->      { a: 1, b: 1, c: 1 }
+    ]                          ->    [ 
+  ['a', 'b', 'c'], 'r')`, () => {
+    let result = sortObjectArrByProps(objArr3, ['a', 'b', 'c'], 'r');
+    expect(result).to.eql([
+      { a: 4, b: 1, c: 1 },
+      { a: 3, b: 2, c: 2 },
+      { a: 2, b: 3, c: 3 },
+      { a: 1 },
+      { a: 1, b: 2, c: 4 },
+      { a: 1, b: 2, c: 3 },
+      { a: 1, b: 2, c: 2 },
+      { a: 1, b: 2, c: 1 },
+      { a: 1, b: 1 },
       { a: 1, b: 1, c: 4 },
       { a: 1, b: 1, c: 3 },
       { a: 1, b: 1, c: 2 },
