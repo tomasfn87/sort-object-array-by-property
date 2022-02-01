f72b96a790f6abc0e8e61449a688dcf4912f542d
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-02-01 02:10:15 -0300

    Changed file 'randomNumberArrayGenerator.ts': reduced the size of the random number generated to make it easier for the function to generate more items with the same value.

--- randomNumberArrayGenerator.ts
+++ randomNumberArrayGenerator.ts
@@ -1,7 +1,7 @@
 export const NewLongArray = (arrLen:number, type:'s'|'n' = 'n'):[] => {
   let longArray:number[] | string[] | any= [];
 
-  const randNum = (() => (parseInt((Math.random() * 3 * 10e4).toFixed())))
+  const randNum = (() => (parseInt((Math.random() * 3 * 100).toFixed())))
 
   for (let i=0; i < arrLen; i++) {
     type !== 's' 
